9a7b4513881421d3449667b46f4f1f4b9c258b06
anzalladd <anzalladd@gmail.com>
2018-10-25 13:48:48 +0700

    installVue

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    
+}
+</script>
+
+<style scoped>
+    
+</style>
